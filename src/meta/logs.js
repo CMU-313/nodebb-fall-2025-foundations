c53fbf7e5e3a671a48ab5bf1eece69bac146402c
heesunp <heesunp@andrew.cmu.edu>
2025-10-11 00:46:33 +0000

    fix: create logs directory

--- src/meta/logs.js
+++ src/meta/logs.js
@@ -8,7 +8,18 @@ const Logs = module.exports;
 Logs.path = path.resolve(__dirname, '../../logs/output.log');
 
 Logs.get = async function () {
-	return await fs.promises.readFile(Logs.path, 'utf-8');
+	try {
+		return await fs.promises.readFile(Logs.path, 'utf-8');
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			// Create logs directory and empty log file if they don't exist
+			const logsDir = path.dirname(Logs.path);
+			await fs.promises.mkdir(logsDir, { recursive: true });
+			await fs.promises.writeFile(Logs.path, '', 'utf-8');
+			return '';
+		}
+		throw err;
+	}
 };
 
 Logs.clear = async function () {

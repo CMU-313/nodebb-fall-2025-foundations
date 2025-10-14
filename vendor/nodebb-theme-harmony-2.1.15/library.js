7128ec50915887e7b6cc61cb80c3765af52b8de2
heesunp <heesunp@andrew.cmu.edu>
2025-10-14 07:04:18 +0000

    fix: remove trailing whitespace from library.js

--- vendor/nodebb-theme-harmony-2.1.15/library.js
+++ vendor/nodebb-theme-harmony-2.1.15/library.js
@@ -44,7 +44,7 @@ async function buildSkins() {
 	try {
 		const plugins = require.main.require('./src/plugins');
 		await plugins.prepareForBuild(['client side styles']);
-		for (const skin of meta.css.supportedSkins) {			 
+		for (const skin of meta.css.supportedSkins) {
 			await meta.css.buildBundle(`client-${skin}`, true);
 		}
 		require.main.require('./src/meta/minifier').killAll();
